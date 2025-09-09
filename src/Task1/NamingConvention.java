fa42b525c0184b8325da51a205662c516bc99658
Adila Dwi Septiani <2482002@unai.edu>
2025-09-09 15:47:06 +0700

    Handson Java syntax practice

--- src/Task1/NamingConvention.java
+++ src/Task1/NamingConvention.java
@@ -3,23 +3,22 @@ package Task1;
 class NamingConvention {
     public static void main(String[] args) {
 
-
-        // Buatlah variabel dengan nama yang mengikuti camelCase
-        // untuk data mahasiswa berikut:
-        // - Adila Dwi Septiani
-        // - 2482002
-        // - 3,65
-        // - status aktif kuliah
-        // - jl.raya way ratai
-        // - jumlah SKS yang diambil
-
-        // Contoh:
-        String namaLengkapMahasiswa = "Andi Wijaya";
-        // Lanjutkan untuk variabel lainnya...
+        String namaLengkapMahasiswa = "Adila Dwi Septiani";
+        String nimMahasiswa = "2482002";
+        double ipkMahasiswa = 3.65;
+        boolean statusAktifKuliah = true;
+        String alamatMahasiswa = "jl. Raya way ratai Kec. Pada Cermin Kab. Pesawaran Lampung";
+        int jumlahSks = 22;
 
         // Tampilkan semua data
         System.out.println("=== DATA MAHASISWA ===");
-        // Lengkapi output di sini
+        System.out.println("Nama Lengkap    : " + namaLengkapMahasiswa);
+        System.out.println("NIM             : " + nimMahasiswa);
+        System.out.println("IPK             : " + ipkMahasiswa);
+        System.out.println("Status Kuliah   : " + (statusAktifKuliah ? "Aktif" : "Tidak Aktif"));
+        System.out.println("Jumlah SKS      : " + jumlahSks);
+
+
     }
 }
 
